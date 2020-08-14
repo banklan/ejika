86b6f919d164579db400f7d1a1a1cb6906b2b554
banklan <banklan2010@gmail.com>
2020-08-14 11:50:32 +0100

    Update settings.py

--- ejika/ejika/settings.py
+++ ejika/ejika/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '3k1r_1fxsv^1!yyad==@l_blvut_nu5rv^5152sn_0w=@j5b*x'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
